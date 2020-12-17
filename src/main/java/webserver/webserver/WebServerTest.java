3ab77aa17b1a0ef69fff7be5dbb2ae5495aecd01
Rusu Teodor <49162774+Artanis24@users.noreply.github.com>
2020-12-17 21:43:06 +0200

    Add files via upload

--- src/main/java/webserver/webserver/WebServerTest.java
+++ src/main/java/webserver/webserver/WebServerTest.java
@@ -12,8 +12,6 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
 
 @FixMethodOrder(MethodSorters.JVM)
 public class WebServerTest {
@@ -70,8 +68,6 @@ public class WebServerTest {
         Assert.assertEquals("Maintenance", webserver.webserverStatus);
     }
 
-    //getFilePath
-
     @Test
     public void getFilePathIndexHtmlRequestedBySlash() {
         String expectedPath = buildPath("", "", "index.html");
@@ -115,23 +111,6 @@ public class WebServerTest {
         Assert.assertEquals("Hey", path.toString());
     }
 
-    @Test
-    public void sendResponseWithNoExceptionThrownPageAcceptedAndCorrectlyDisplayed() throws IOException {
-        String expectedPath = buildPath("", "", "index.html");
-
-        WebServer webserver = buildDefaultWebServer();
-        Socket clientSocket;
-        try (ServerSocket serverSocket = new ServerSocket(CLIENT_PORT)) {
-            clientSocket = serverSocket.accept();
-        }
-        Path path = Paths.get(expectedPath);
-
-        OutputStream out = clientSocket.getOutputStream();
-        //trebuie accesat localhost:8080 in browser ca sa se finalizeze testul
-        webserver.sendResponse(out, "200 OK", "text/html", Files.readAllBytes(path));//html only
-        out.close();
-    }
-
     @Test(expected = IOException.class)
     public void sendResponseWithExceptionThrown() throws IOException {
         String expectedPath = buildPath("", "", "index.html");
@@ -144,8 +123,7 @@ public class WebServerTest {
         Path path = Paths.get(expectedPath);
 
         OutputStream out = clientSocket.getOutputStream();
-        out.close();//exception is given because the client disconnected
-        //trebuie accesat localhost:8080 in browser ca sa se finalizeze testul
+        out.close();
         webserver.sendResponse(out, "200 OK", "text/html", Files.readAllBytes(path));//html only
     }
 
@@ -155,161 +133,9 @@ public class WebServerTest {
         webserver.handleClient(null);
     }
 
-    //cant force it to not enter the while with the readline -- this is impossible since when you connect, you automatically send some request
-
-    /*@Test
-    public void handleClientWithRunningServerAndRequestForIndexPage() throws IOException {
-        //connect to localhost:8080
-        //request the index
-        WebServer webserver = new WebServer(WEBSERVER_PORT + 1, WEBSERVER_ROOT_DIRECTORY, WEBSERVER_MAINTENANCE_DIRECTORY, "Maintenance");
-        try (ServerSocket serverSocket = new ServerSocket(CLIENT_PORT + 2)) {
-            Socket clientSocket;
-
-            clientSocket = serverSocket.accept();
-            webserver.handleClient(clientSocket); //html
-
-            clientSocket = serverSocket.accept();
-            webserver.handleClient(clientSocket); //css
-
-            clientSocket = serverSocket.accept();
-            webserver.handleClient(clientSocket); //favicon
-        }
-
-        //Due to how the handlerequests attribute is constructed, the first character will be a space
-        String index = buildPath("", "", "index.html");
-        String css = buildPath("", "", "index.css");
-        String ico = buildPath("", "favicon.ico");
-
-        String expectedRes = " " + index + " " + css + " " + ico;
-
-        Assert.assertEquals(expectedRes, webserver.handledRequests);
-    }*/
-
-    @Test
-    public void handleClientWithRunningServerAndRequestForUnknownPage() throws IOException {
-        //connect to localhost:8080
-        WebServer webserver = buildDefaultWebServer();
-        webserver.handledRequests = "";
-        ServerSocket serverSocket = new ServerSocket(CLIENT_PORT);
-
-        Socket clientSocket = serverSocket.accept();
-        webserver.handleClient(clientSocket); //html
-
-        clientSocket = serverSocket.accept();
-        webserver.handleClient(clientSocket); //css
-
-        clientSocket = serverSocket.accept();
-        webserver.handleClient(clientSocket); //favicon
-
-        //Due to how the handlerequests attribute is constructed, the first character will be a space
-        String index = buildPath("", "", "bubu.html");
-        String css = buildPath("", "", "notfound.css");
-        String ico = buildPath("", "favicon.ico");
-
-        String expectedRes = " " + index + " " + css + " " + ico;
-
-        Assert.assertEquals(expectedRes, webserver.handledRequests);
-
-        webserver.handledRequests = "";
-    }
-
-    @Test
-    public void handleClientWithMaintenanceServerAndRequestForIndexPage() throws IOException {
-        //connect to localhost:8080
-        //request the index
-
-        WebServer webserver = new WebServer(WEBSERVER_PORT + 1, WEBSERVER_ROOT_DIRECTORY, WEBSERVER_MAINTENANCE_DIRECTORY, "Maintenance");
-        webserver.handledRequests = "";
-        ServerSocket serverSocket = new ServerSocket(CLIENT_PORT);
-
-        Socket clientSocket = serverSocket.accept();
-        webserver.handleClient(clientSocket); //html
-
-        clientSocket = serverSocket.accept();
-        webserver.handleClient(clientSocket); //css
-
-        clientSocket = serverSocket.accept();
-        webserver.handleClient(clientSocket); //favicon
-
-        String index = buildPath("", "", "index.html");
-        String css = buildPath("", "", "maintenance.css");
-        String ico = buildPath("", "favicon.ico");
-
-        String expectedRes = " " + index + " " + css + " " + ico;
-
-        Assert.assertEquals(expectedRes, webserver.handledRequests);
-
-        webserver.handledRequests = "";
-    }
-
-    @Test
-    public void handleClientWithStoppedServerAndRequestForIndexPage() throws IOException {
-        //connect to localhost:8080
-        //request the index
-        WebServer webserver = new WebServer(WEBSERVER_PORT + 1, WEBSERVER_ROOT_DIRECTORY, WEBSERVER_MAINTENANCE_DIRECTORY, "Stopped");
-        webserver.handledRequests = "";
-        ServerSocket serverSocket = new ServerSocket(CLIENT_PORT);
-
-        Socket clientSocket = serverSocket.accept();
-        webserver.handleClient(clientSocket); //html
-
-        clientSocket = serverSocket.accept();
-        webserver.handleClient(clientSocket); //css
-
-        clientSocket = serverSocket.accept();
-        webserver.handleClient(clientSocket); //favicon
-
-        //Due to how the handlerequests attribute is constructed, the first character will be a space
-        String index = buildPath("", "", "index.html");
-        String css = buildPath("", "", "serverdown.css");
-        String ico = buildPath("", "favicon.ico");
-
-        String expectedRes = " " + index + " " + css + " " + ico;
-
-        Assert.assertEquals(expectedRes, webserver.handledRequests);
-
-        webserver.handledRequests = "";
-    }
-
-    @Test
-    public void handleClientWithRunningServerAndRequestForIndexPageClientDisconnectingIOExceptionThrown() throws IOException {
-        //connect to localhost:8080
-        //request the index
-
-        WebServer webserver = buildDefaultWebServer();
-        ServerSocket serverSocket = new ServerSocket(CLIENT_PORT);
-
-        Socket clientSocket = serverSocket.accept();
-        clientSocket.close();
-        webserver.handleClient(clientSocket); //htm
-    }
-
-
-    @Test
-    public void handleOneRequestWithRunningServerHandlingKnownPageRequestNoExceptionOccuring() {
-        //connect to localhost:8080
-        //request the index
-
-        WebServer webserver = buildDefaultWebServer();
-        webserver.handledRequests = "";
-        webserver.handleOneRequest(); //html
-        webserver.handleOneRequest(); //css
-        webserver.handleOneRequest(); //favicon
-
-        String index = buildPath("", "", "index.html");
-        String css = buildPath("", "", "index.css");
-        String ico = buildPath("", "favicon.ico");
-
-        String expectedRes = " " + index + " " + css + " " + ico;
-
-        Assert.assertEquals(expectedRes, webserver.handledRequests);
-        webserver.handledRequests = "";
-    }
-
     @Test
     public void handleOneRequestWithRunningServerIOExceptionWhenCreatingAServerSocket() {
 
-        //if the port is still established at the time of execution, you will see the message "Could not listen on port: xxxxxx"
         WebServer webserver = new WebServer(CLIENT_PORT, WEBSERVER_ROOT_DIRECTORY, WEBSERVER_MAINTENANCE_DIRECTORY, WEBSERVER_STATUS);
         WebServer webserver2 = new WebServer(CLIENT_PORT, WEBSERVER_ROOT_DIRECTORY, WEBSERVER_MAINTENANCE_DIRECTORY, WEBSERVER_STATUS);
         webserver.handleOneRequest(); //html
@@ -319,45 +145,4 @@ public class WebServerTest {
 
     }
 
-    @Test
-    public void handleOneRequestWithRunningServerIOExceptionWhenTwoClientsConnectAtSameServer() throws BrokenBarrierException, InterruptedException {
-
-        WebServer webserver = buildDefaultWebServer();
-        final CyclicBarrier gate = new CyclicBarrier(2 + 1);
-
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                try {
-                    gate.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (BrokenBarrierException e) {
-                    e.printStackTrace();
-                }
-                webserver.handleOneRequest(); //html
-
-            }
-        });
-
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                try {
-                    gate.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (BrokenBarrierException e) {
-                    e.printStackTrace();
-                }
-                webserver.handleOneRequest(); //html
-            }
-        });
-        t1.start();
-        t2.start();
-
-        gate.await();
-    }
 }
